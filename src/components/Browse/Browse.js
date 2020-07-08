8ccf324dfa9d313ae52d040fd0a045c366d5675c
Reelshrimpboat <Camneelycreative@gmail.com>
2020-07-07 20:45:30 -0500

    Search works on Broswse Page

--- src/components/Browse/Browse.js
+++ src/components/Browse/Browse.js
@@ -6,27 +6,80 @@ import BrowseTea from '../TeaItems/TeaItemBrowse/TeaItemBrowse'
 
 
 class Browse extends Component {
+ 
+  state = {
+    query: '',
+    results: [],
+  }
 
+  searchCheck = () => {
+
+    let newReturn = []
+
+    if(this.state.query !== ''){
+    newReturn = this.props.teas.filter((tea) => tea.name.includes(this.state.query) === true)
+    }
+    else{
+    newReturn = []
+    }
+    this.setState({
+        results: newReturn,
+    }, () => {
+        console.log('new results:', this.state.results)
+    })
+ }
+
+ handleInputChange = () => {
+   this.setState({
+     query: this.search.value
+   }, () => {
+       this.searchCheck()
+   })
+ }
   async componentDidMount() {
         await this.props.dispatch({type: 'FETCH_USERS_TEAS'})
+        this.searchCheck()
     }
   
  render() {
    const { match } = this.props;
    return (
     <section>
+      <div>
+        <input
+         placeholder="Search for..."
+         ref={input => this.search = input}
+         onChange={this.handleInputChange}
+        />
+      </div>
+      <div>
         {this.props.teas &&
-          this.props.teas.map((tea) => 
+        <>
+          {this.state.query === '' ?
+            this.props.teas.map((tea) =>
               <Link key={tea.id} to={{
                 pathname: `${match.url}/tea`,
                 tea: tea,
                 state: { modal: true }
               }}>
-              <BrowseTea tea={tea} id={tea.id} rating={tea.rating} />
+                <BrowseTea tea={tea} id={tea.id} rating={tea.rating} />
+              </Link>
+            )
+          :
+          this.state.results.map((tea) =>
+              <Link key={tea.id} to={{
+                pathname: `${match.url}/tea`,
+                tea: tea,
+                state: { modal: true }
+              }}>
+                <BrowseTea tea={tea} id={tea.id} rating={tea.rating} />
               </Link>
           )
         }
-     </section>
+        </>
+        }
+      </div>
+    </section>
    )
  }
 }
